91a91bbfdd6680ebe37d057d91de68370b7a494e
haruki7049 <tontonkirikiri@gmail.com>
2025-06-20 06:15:05 +0900

    Update

--- src/bin/mvm.rs
+++ src/bin/mvm.rs
@@ -1,9 +1,19 @@
 use clap::Parser;
 use std::path::PathBuf;
-use music_virtual_machine;
+use std::path::Path;
+use std::ffi::OsStr;
+use chrono::Local;
+use chrono::DateTime;
+use directories::ProjectDirs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: CLIArgs = CLIArgs::parse();
+    let output_path: PathBuf = if args.output.is_none() {
+        output_path(&args.path)?
+    } else {
+        args.output.unwrap()
+    };
+
     let music_data: music_virtual_machine::MusicData = music_virtual_machine::parse(&args.path)?;
 
     let spec = hound::WavSpec {
@@ -13,7 +23,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         sample_format: music_data.sample_format.into(),
     };
 
-    let mut writer = hound::WavWriter::create(args.output, spec)?;
+    let mut writer = hound::WavWriter::create(output_path, spec)?;
 
     for bit in music_data.bits {
         writer.write_sample(bit as i16)?;
@@ -31,5 +41,18 @@ struct CLIArgs {
     path: PathBuf,
 
     /// Output wave file path
-    output: PathBuf,
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+}
+
+fn output_path(mvm_path: &PathBuf) -> Result<PathBuf, Box<dyn std::error::Error>> {
+    let project_dirs: ProjectDirs = ProjectDirs::from("dev", "haruki7049", "mvm").ok_or("Failed to get project dir for dev.haruki7049.mvm")?;
+    let cache_dir: &Path = project_dirs.cache_dir();
+    let date: DateTime<Local> = Local::now();
+    let toml_path: &OsStr = mvm_path.file_stem().ok_or("Failed to get filename of Music VM toml file")?;
+    let result: PathBuf = format!("{}/{}-{}.wav", cache_dir.display(), toml_path.display(), date.timestamp_millis()).into();
+
+    std::fs::create_dir_all(cache_dir)?;
+
+    Ok(result)
 }
